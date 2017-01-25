6134191e2bbc89b5c384f5cd5186832d5c347dff
patricio <patriciohc.0@gmail.com>
2017-01-25 09:14:05 -0600

    controllers

--- server/controllers.js
+++ server/controllers.js
@@ -1,23 +1,35 @@
 'use strict'
 const fs = require('fs');
 
-const dirFiles = "datos_txt"
+const dirFiles = "./datos_txt"
 
 function getListTxt(req, res) {
     fs.readdir(dirFiles, (err, files) => {
         if (err) {
+            console.log(err);
             throw err;
         }
-        files.map( file => {
-            //return 
-        }).filter( file => {
-            //return fs.statSync(file).isFile();
-        }).forEach( file => {
-            var ext = _self.path.extname(file);
-            var name_complete = _self.path.basename(file);
-            var name_simple = name_complete.replace(ext, "");
-            console.log("---> %s : %s (%s)", name_complete, name_simple, ext);
-        });
+        return res.status(200).send(files);
+        // files.map( file => {
+        //     console.log(file);
+        //     //return
+        // }).filter( file => {
+        //     console.log(file);
+        //     //return fs.statSync(file).isFile();
+        // }).forEach( file => {
+        //     console.log(file);
+        //     //var ext = _self.path.extname(file);
+        //     //var name_complete = _self.path.basename(file);
+        //     //var name_simple = name_complete.replace(ext, "");
+        //     console.log("---> %s : %s (%s)", name_complete, name_simple, ext);
+        // });
+    });
+}
+
+function getDataFile(req, res) {
+    fs.readFile(dirFiles + "/" + req.body.nameFiles, (err, data) => {
+      if (err) throw err;
+      console.log(data);
     });
 }
 
--- server/routers.js
+++ server/routers.js
@@ -6,9 +6,10 @@ const controllers = require('./controllers');
 //const middleware = require('../middleware');
 
 // user
-api.get('/listTxt/', controllers.getListTxt);
+api.get('/listText/', controllers.getListTxt);
+api.post('/filetext/',controllers.getDataFile);
 //api.get('/user/:id', controllers.getUser);
-//api.post('/user/',controllers.createUSer);
+
 //api.get('/cat-atributo/:id', catProductoCtrl.getCatProducto);
 
 module.exports = api;
