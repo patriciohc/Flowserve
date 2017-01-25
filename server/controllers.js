a6c68acfc40ecdb626a8f9b1b033ed50d264002c
patricio <patriciohc.0@gmail.com>
2017-01-25 17:17:48 -0600

    se comenta conexion sql server

--- server/controllers.js
+++ server/controllers.js
@@ -1,19 +1,19 @@
 'use strict'
-const Connection = require('tedious').Connection;
+//const Connection = require('tedious').Connection;
 const configDB = require('./configDB');
 const fs = require('fs');
 const models = require('./models');
 var Request = require('tedious').Request;
 
-var connection = new Connection(configDB.sqlServer);
+//var connection = new Connection(configDB.sqlServer);
 
-connection.on('connect', function(err) {
-    if (err){
-        console.log("error: " + err);
-    } else {
-        console.log("Connected");
-    }
-});
+// connection.on('connect', function(err) {
+//     if (err){
+//         console.log("error: " + err);
+//     } else {
+//         console.log("Connected");
+//     }
+// });
 
 const dirFiles = "E:/datos_txt"
 
@@ -29,30 +29,34 @@ function getListTxt(req, res) {
 }
 
 function getDataFile(req, res) {
-    var nameFile = dirFiles + "/" + "33_NPG_Invoices_303479_Thru_303501_On_2016-11-24"
+    var nameFile = dirFiles + "/" + "33_NPG_Invoices_303479_Thru_303501_On_2016-11-24.txt"
     fs.readFile(nameFile, 'utf8', (err, data) => {
         if (err) throw err;
-            var lines = data.split("\n");
-            console.log(data);
-            res.status(200).send(data);
+            var facturas = data.split("XXXINICIO");
+            facturas = facturas.map( item => {
+                return item.split("\r\n");
+            });
+            //var lines = data.split("\r\n");
+            //console.log(data);
+            res.status(200).send(facturas);
     });
 }
 
 function testDB(req, res){
-    var request = new Request("select * from table_test", function(err, rowCount) {
-        if (err) {
-            console.log(err);
-            throw err;
-            return res.status(200).send("error");
-        }
-        //return res.status(200).send(rowCount);
-    });
+    // var request = new Request("select * from table_test", function(err, rowCount) {
+    //     if (err) {
+    //         console.log(err);
+    //         throw err;
+    //         return res.status(200).send("error");
+    //     }
+    //     //return res.status(200).send(rowCount);
+    // });
 
-    request.on('row', columns => {
-        return res.status(200).send(columns);
-    });
+    // request.on('row', columns => {
+    //     return res.status(200).send(columns);
+    // });
 
-    connection.execSql(request);
+    // connection.execSql(request);
 }
 
 
