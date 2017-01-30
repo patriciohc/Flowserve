9b0f364e154431bc3a4f0967b7691983798484b6
patricio <patriciohc.0@gmail.com>
2017-01-29 23:15:51 -0600

    separacion de facturas, procesamiento de txt

--- server/controllers/EIS.js
+++ server/controllers/EIS.js
@@ -1,40 +1,53 @@
-// configuaraciones de las base de datos
-const configDB = require('../configDB');
-// conexcion sql server
-const Connection = require('tedious').Connection;
-var Request = require('tedious').Request;
-var TYPES = require('tedious').TYPES
+// // configuaraciones de las base de datos
+// const configDB = require('../configDB');
+// // conexcion sql server
+// const Connection = require('tedious').Connection;
+// var Request = require('tedious').Request;
+// var TYPES = require('tedious').TYPES
+//
+// var connection = new Connection(configDB.sqlServer);
+//
+// connection.on('connect', function(err) {
+//     if (err){
+//         console.log("error: " + err);
+//     } else {
+//         console.log("Connected");
+//     }
+// });
 
-var connection = new Connection(configDB.sqlServer);
+function getDatos(sku) {
 
-connection.on('connect', function(err) {
-    if (err){
-        console.log("error: " + err);
-    } else {
-        console.log("Connected");
-    }
-});
-
-function testDB(sku, success) {
-    var request = new Request("fsgTLXPartMaster", function(err, rowCount) {
-        if (err) {
-            success(null);
-        }
-    });
-
-    request.addParameter('PartCode', TYPES.VarChar, sku);
-    request.addParameter('OrganizationKey', TYPES.Int, 1);
-
-    // request.on('row', rows => {
-    //     console.log(rows);
-    //     return res.status(200).send(rows);
+    // return new Promise( (resolve, reject) => {
+    //
+    //     var request = new Request("fsgTLXPartMaster", function(err, rowCount) {
+    //         if (err) {
+    //             success(null);
+    //         }
+    //     });
+    //
+    //     request.addParameter('PartCode', TYPES.VarChar, sku);
+    //     request.addParameter('OrganizationKey', TYPES.Int, 1);
+    //
+    //     // request.on('row', rows => {
+    //     //     console.log(rows);
+    //     //     return res.status(200).send(rows);
+    //     // });
+    //
+    //     request.on('doneProc', (rowCount, more, returnStatus, rows) => {
+    //         var marca = rows.find( item => item.colName == "PartManufacturer");
+    //         var modelo = rows.find( item => item.colName == "MfgPartNumber");
+    //         resolve({marca, modelo});
+    //     });
     // });
 
-    request.on('doneProc', (rowCount, more, returnStatus, rows) => {
-        var marca = rows.find( item => item.colName == "PartManufacturer");
-        var modelo = rows.find( item => item.colName == "MfgPartNumber");
-        success({marca, modelo});
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, parseInt(Math.random() * 3000), {marca: sku, modelo: sku});
     });
 
-    connection.callProcedure(request);
+    //connection.callProcedure(request);
 }
+
+
+module.exports = {
+    getDatos,
+};
--- server/controllers/facturas.js
+++ server/controllers/facturas.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // consulta base de datos sqlServer
-//const EIS = require("./EIS.js");
+const EIS = require("./EIS.js");
 // lectura de archivos de directorios y archivos de texto
 const fs = require('fs');
 // lectura de excel
@@ -9,144 +9,186 @@ const XLSX = require('xlsx');
 const excel = require('../excel')
 
 //const dirFiles = "E:/datos_txt"
-const dirFiles = "./datos_txt";
+const dirFacturas = "./datos_txt";
+const dirFacturasNacionales = "./facturas_nacionales";
 
+// carga informacion del excel en formato json
 var jsonExcel = (function(){
     var workbook = XLSX.readFile("./info_excel/Base_avance_para_IT.xlsx");
     //return excel.readExcel(workbook);
     var sheets = excel.readExcel(workbook);
     return sheets[0];
 })();
-
-
-fs.watch('./datos_txt', {encoding: 'utf8'}, (eventType, filename) => {
+/** esta a la eschucha de nuevos txt en el directorios de faturas */
+fs.watch(dirFacturas, {encoding: 'utf8'}, (eventType, filename) => {
     if (eventType == "rename")
-        if (filename)
-            procesarFactura(filename);
-        
+        if (filename){
+            var file = dirFacturas + "/" + filename;
+            procesarTxt(filename);
+        }
 });
-
-
-function getListTxt(req, res) {
-    fs.readdir(dirFiles, (err, files) => {
-        if (err) {
-            return res.status(200).send(null);
-            console.log(err);
-        }
-        var lista = [];
-        for (var i in files) {
-            var archivo = {};
-            var nameFile =  dirFiles +"/"+ files[i];
-            var texto = fs.readFileSync(nameFile, 'utf8');
-            archivo.nombre = files[i];
-            archivo.cantidad = texto.split("XXXINICIO").filter(item => item != item.trim()).length;
-            lista.push(archivo);
-        }
-        return res.status(200).send(lista);
-    });
+/** procesa los txt que se encuentran en el directorio */
+(function procesarDirectorio(){
+    console.log("procesando");
+    var list = fs.readdirSync(dirFacturas);
+    for (var i in list){
+        var file = dirFacturas + "/" + list[i];
+        console.log(file);
+        procesarTxt(file);
+    }
+})();
+/**
+* separa en facturas nacionales y extranjeras, en las facturas extranjeras agrega
+* la informacion requerida, las facturas nacionales las guarda en un nuevo txt
+* @param {string} nameTxt - nombre completo del archivo txt, incluyendo la ruta
+*/
+function procesarTxt(nameTxt){
+    var facturas = convertTxtToJson(nameTxt);
+    if (!facturas) return;
+    facturas = separarFacturas(facturas);
+    var nombreNacionales = nameTxt.split(".")[0] + "_A1" + ".txt";
+    writeFile(facturas.nacionales, nombreNacionales, dirFacturasNacionales);
+    addInfoFactura(facturas.extranjeras)
+    .then( values => writeFile(facturas.extranjeras, nameTxt, dirFacturas))
 }
-
-function procesarFactura(filename){
-
+/**
+* separa en facturas nacionales y extranjeras
+* @param {Array} facturas - array de facturas
+*/
+function separarFacturas(facturas) {
+    var s = {
+        nacionales: [],
+        extranjeras: []
+    };
+    for (var i in facturas ){
+        var lugar = facturas[i].receptor[2].Pais;
+        if (lugar == "MEXICO"){
+            s.nacionales.push(facturas[i]);
+        } else {
+            s.extranjeras.push(facturas[i]);
+        }
+    }
+    return s;
 }
-
-
-function getFacturas(req, res) {
-    var nameFile = dirFiles + "/" + req.body.nameFile;
-    //var nameFile = dirFiles + "/" + "33_NPG_Invoices_303479_Thru_303501_On_2016-11-24.txt"
-    fs.readFile(nameFile, 'utf8', (err, data) => {
-        if (err) throw err;
-        var facturas = data.split("XXXINICIO");
-        facturas = facturas.filter(item => item != item.trim());
-        facturas = facturas.map( item => {
-            return item.split("\r\n\r\n")
-                .map( item => {
-                    var elementos = item.split("\r\n");
-                    var dic = {};
-                    for (var i = 0; i < elementos.length; i++) {
-                        var item = elementos[i].trim();
-                        if (item == "") continue;
-                        var key = item.split(" ")[0];
-                        var value = item.substring(key.length, item.length).trim();
-                        if (key == "TpoCodigo1"){
-                            var infoHead = procesarHead(item);
-                            var rows = [];
-                            i++;
-                            while(i < elementos.length){
-                                rows.push(procesarRow(elementos[i], infoHead));
-                                i++;
-                            }
-                            dic["productos"] =  { head: infoHead, rows: rows } ;
-                        } else {
-                            dic[key] = value;
-                        }
+/**
+* agrega la informacion requerida para facturas extranjeras
+* @param {Array} - array de facturas
+* @return {Promise} promises
+*/
+function addInfoFactura(facturas){
+    var promises = []
+    for (var i in facturas){
+        addSeccionManual(facturas[i]);
+        complementarInfoPrductos(facturas[i].receptor[2].productos, promises)
+    }
+    return Promise.all(promises);
+}
+/**
+* procesa el txt y regresa un json con las facturas que contiene
+* @param {string} nameFile - nombre completo de archivo
+*/
+function convertTxtToJson(nameFile) {
+    //var nameFile = dirFiles + "/" + "33_NPG_Invoices_303479_Thru_303501_On_2016-11-24.txt";
+    var texto = fs.readFileSync(nameFile, 'utf8');
+    if (!texto) return;
+    var facturas = texto.split("XXXINICIO");
+    facturas = facturas.filter(item => item != item.trim());
+    facturas = facturas.map( item => {
+        return item.split("\r\n\r\n")
+            .map( item => {
+                var elementos = item.split("\r\n");
+                var dic = {};
+                for (var i = 0; i < elementos.length; i++) {
+                    var item = elementos[i].trim();
+                    if (item == "") continue;
+                    var key = item.split(" ")[0];
+                    var value = item.substring(key.length, item.length).trim();
+                    if (key == "TpoCodigo1") {
+                        var infoHead = procesarHead(item);
+                        var rows = [];
+                        while(++i < elementos.length)
+                            rows.push(procesarRow(elementos[i], infoHead));
+                        dic["productos"] =  { head: infoHead, rows: rows } ;
+                    } else {
+                        dic[key] = value;
                     }
-                    return dic;
-                });
-        });
-
-        var facturasProcesadas = [];
-        for (var i in facturas) {
-            var factura = facturas[i];
-            if (factura.length < 2) continue;
-            var facturaProcesada = {
-                factura: [], // datos facutura
-                emisor:[], // datos emisor
-                receptor: [], // datos receptor
-                otros: [] //  XXXFINDETA
-            };
-            var parteAcutal = null;  // indicara la parte actual de la factura
-            for (var j in factura ) {
-                var itemFactura = factura[j];
-                var key = Object.keys(itemFactura)[0];
-                if (typeof key == "undefined") continue;
-                switch(key){
-                    case "NumeroInterno":
-                        parteAcutal = "factura"
-                        break;
-                    case "RFCEmisor":
-                        parteAcutal = "emisor"
-                        break;
-                    case "RFCRecep":
-                        parteAcutal = "receptor"
-                        break;
-                    case "XXXFINDETA":
-                        parteAcutal = "otros"
-                        break;
                 }
-
-                switch(parteAcutal){
-                    case "factura":
-                        facturaProcesada.factura.push(itemFactura);
-                        break;
-                    case "emisor":
-                        facturaProcesada.emisor.push(itemFactura);
-                        break;
-                    case "receptor":
-                        facturaProcesada.receptor.push(itemFactura);
-                        break;
-                    case "otros":
-                        facturaProcesada.otros.push(itemFactura);
-                        break;
-                }
-            }
-            var nuevosDatos = {
-                cceNExpConfiable: "",
-                cceCertOrig: "",
-                cceNCertOrig: "",
-                cceVersion: "",
-                cceTipoOp: "",
-                cceClavePed: "",
-            }
-            facturaProcesada.receptor.push(nuevosDatos);
-            facturasProcesadas.push(facturaProcesada);
-        }
-
-        return res.status(200).send(facturasProcesadas);
+                return dic;
+            });
     });
+
+    var facturasProcesadas = [];
+    var arrayPromises = [];
+    for (var i in facturas) {
+        var factura = facturas[i];
+        if (factura.length < 2) continue;
+        var facturaProcesada = {
+            factura: [], // datos facutura
+            emisor:[], // datos emisor
+            receptor: [], // datos receptor
+            otros: [] //  XXXFINDETA
+        };
+        var parteAcutal = null;  // indicara la parte actual de la factura
+        for (var j in factura ) {
+            var itemFactura = factura[j];
+            var key = Object.keys(itemFactura)[0];
+            if (typeof key == "undefined") continue;
+            switch(key){
+                case "NumeroInterno":
+                    parteAcutal = "factura"
+                    break;
+                case "RFCEmisor":
+                    parteAcutal = "emisor"
+                    break;
+                case "RFCRecep":
+                    parteAcutal = "receptor"
+                    break;
+                case "XXXFINDETA":
+                    parteAcutal = "otros"
+                    break;
+            }
+            switch(parteAcutal){
+                case "factura":
+                    facturaProcesada.factura.push(itemFactura);
+                    break;
+                case "emisor":
+                    facturaProcesada.emisor.push(itemFactura);
+                    break;
+                case "receptor":
+                    facturaProcesada.receptor.push(itemFactura);
+                    break;
+                case "otros":
+                    facturaProcesada.otros.push(itemFactura);
+                    break;
+            }
+        }
+        facturasProcesadas.push(facturaProcesada);
+    }
+    return facturasProcesadas;
 }
-
-
+/**
+* agrega las claves para la informacion que se agregara manualmente
+* @param {json} factura - factura
+*/
+function addSeccionManual(factura){
+    var nuevosDatos = {
+        cceNExpConfiable: "",
+        cceCertOrig: "",
+        cceNCertOrig: "",
+        cceVersion: "",
+        cceTipoOp: "",
+        cceClavePed: "",
+    }
+    factura.receptor.push(nuevosDatos);
+}
+/**
+* dentro de cada factura hay un aparatado donde se encuetran los productos con
+* con su descripcion en forma de columnas, esta funcion procesa el nombre de
+* las columnas
+* @param {string} linea - linea de texto
+* @return {json} array de json { nombre: nombre de la columna,
+* posicion: numero de columna en la que se encuentra dentro del txt  }
+*/
 function procesarHead(linea){
     var head = [];
     var array = linea.split(" ")
@@ -161,24 +203,29 @@ function procesarHead(linea){
     }
     return head;
 }
-
+/**
+* dentro de cada factura hay un aparatado donde se encuetran los productos con
+* con su descripcion en forma de columnas, esta funcion procesa cada producto
+* @param {string} linea - linea de texto
+* @return {json}
+*/
 function procesarRow(linea, head){
     var row = {};
     for (var i = 0; i < head.length - 1; i++){
         var inicio = head[i].posicion;
         var fin = head[i + 1].posicion;
         row[head[i].nombre] = linea.substring(inicio, fin).trim();
-        //row.push(linea.substring(inicio, fin).trim());
     }
     row[head[head.length-1].nombre] = linea.substring(head[head.length-1].posicion, linea.length).trim();
-    //row.push(linea.substring(head[head.length-1].posicion, linea.length).trim());
-    complementarInfoPrducto(row, head);
     return row;
 }
-
-
-function saveText(req, res) {
-    var facturas = req.body.facturas;
+/**
+* guarda el texto en un archivo
+* @param {string} facturas - facturas en json
+* @param {string} nombreTxt - nmbre del archivo
+* @param {string} directorio - directorio donde se guardara
+*/
+function writeFile(facturas, nombreTxt, directorio) {
     var white = "                                                                                                                        ";
     var inicio = "XXXINICIO";
     var texto = "";
@@ -193,9 +240,11 @@ function saveText(req, res) {
                 var itemSeccion = seccion[keyItemSeccion]
                 for (var keyImteSeccion in itemSeccion) {
                     if (keyImteSeccion == "productos") {
-                        texto += writeProductos(itemSeccion[keyImteSeccion]);
+                        texto += convertProductosJsonToTxt(itemSeccion[keyImteSeccion]);
                     } else {
-                        texto += keyImteSeccion + white.substring(0, 17 - keyImteSeccion.length) + itemSeccion[keyImteSeccion] + "\r\n";
+                        texto += keyImteSeccion
+                            + white.substring(0, 17 - keyImteSeccion.length)
+                            + itemSeccion[keyImteSeccion] + "\r\n";
                     }
                 }
                 texto += "\r\n";
@@ -204,21 +253,25 @@ function saveText(req, res) {
         }
     }
 /////////////3032
-    fs.writeFile( dirFiles + "/" + 'message.txt', texto, (err) => {
+    fs.writeFile( directorio + "/" + nombreTxt, texto, (err) => {
         if (err) {
-            res.status(200).send({"message": `error al guardar el archivo ${err}`});
+            console.log(`error al guardar el archivo ${err}`);
         };
-        res.status(200).send({"message": "suscces" });
+            console.log("archivo se guardo correctamente");
     });
-
 }
-
-function writeProductos(productos) {
+/**
+* convierte lo productos que se encuentran en formato json a texto para ser escritos
+* en el text
+* @param {json} productos - productos
+*/
+function convertProductosJsonToTxt(productos) {
     var white = "                                                                                                                                                                                                                                                                                          ";
     var texto = "";
     for (var i = 0; i < productos.head.length-1; i++) {
         var espcioDisponible = productos.head[i+1].posicion - productos.head[i].posicion;
-        texto += productos.head[i].nombre + white.substring(0, espcioDisponible - productos.head[i].nombre.length);
+        texto += productos.head[i].nombre
+            + white.substring(0, espcioDisponible - productos.head[i].nombre.length);
     }
     texto += productos.head[productos.head.length-1].nombre;
     texto += "\r\n";
@@ -234,8 +287,12 @@ function writeProductos(productos) {
     }
     return texto;
 }
-
-function complementarInfoPrducto (producto, head) {
+/**
+* complementa la informacion que se requiere para cada producto
+* @param {json} producto - producto
+* @param {json} head - descripcion de las columnas
+*/
+function complementarInfoPrducto(producto, head) {
 
     var codigo = producto.VlrCodigo1
 
@@ -265,6 +322,58 @@ function complementarInfoPrducto (producto, head) {
     // datos EIS
     checkItemEIS("cceMarca");
     checkItemEIS("cceModelo");
+}
+/**
+* complementa la informacion para los productos recibidos
+* @param {json} productos - array de productos
+* @param {Array} arrayPromises - debido a que se ejecutan consultas sqlServer
+* asincronas, es necesario para determinar el momento en que se han ejecutado todas
+*/
+function complementarInfoPrductos(productos, arrayPromises){
+    for (var i in productos.rows){
+        var p = productos.rows[i];
+        complementarInfoPrducto(p, productos.head);
+        console.log("solicitando: " + p.VlrCodigo1);
+        var promise = EIS.getDatos(p.VlrCodigo1).then(function(datos){
+            console.log(datos);
+            p.cceMarca = datos.marca;
+            p.cceModel = datos.modelo;
+        });
+        arrayPromises.push(promise);
+    }
+}
+
+//////// funciones en escucha de peticiones http ///////
+
+/**
+* obtiene una lista de txt en el directorio
+* @return lista de archivos txt en el directorio { nombre, cantidad: cantidad de facturas }
+*/
+function getListTxt(req, res) {
+    fs.readdir(dirFiles, (err, files) => {
+        if (err) {
+            return res.status(200).send(null);
+            console.log(err);
+        }
+        var lista = [];
+        for (var i in files) {
+            var archivo = {};
+            var nameFile =  dirFiles +"/"+ files[i];
+            var texto = fs.readFileSync(nameFile, 'utf8');
+            archivo.nombre = files[i];
+            archivo.cantidad = texto.split("XXXINICIO").filter(item => item != item.trim()).length;
+            lista.push(archivo);
+        }
+        return res.status(200).send(lista);
+    });
+}
+/**
+* regresa las facturas en el txt
+*/
+function getFacturas(req, res) {
+    var nameFile = dirFacturas + "/" + req.body.nameFile;
+    var facturas = convertTxtToJson(nameFile);
+    res.status(200).send(facturas);
 }
 
 module.exports = {
