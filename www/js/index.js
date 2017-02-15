43e5175c6f884711f5f1a6583908704f510d0bf2
maniangel1990 <alukard327@gmail.com>
2017-02-14 18:08:19 -0600

    Cambios

--- www/js/index.js
+++ www/js/index.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
         if (infoUser.user.rolUser == "admin") 
             $("#idbtnReguser").show();
         else
-            $("#idbtnReguser").hide();
+            $("#idbtnReguser").show();
         $("body").css("background", "white")
         document.getElementById("idloguin").style.display = "none";
         document.getElementById("divPrincipal").style.display = "block";
